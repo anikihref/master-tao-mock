991cd5316bc3dbbfcfd43ebb34bbcfd99f1f2f77
Anton Stecyura <grandwar39@gmail.com>
2022-05-14 23:48:15 +0300

    style delivery block

--- src/script/slick-slider.js
+++ src/script/slick-slider.js
@@ -14,7 +14,7 @@ $('.header-slider').slick({
     {
       breakpoint: 1440,
       settings: {
-        appendArrows: '',
+        arrows: false,
       },
     },
   ],
@@ -24,4 +24,44 @@ $('.advantages-slider ').slick({
   dots: true,
   autoplay: true,
   arrows: false,
+});
+
+$('.delivery__slider').slick({
+  slidesToScroll: 1,
+  slidesToShow: 3,
+  infinite: false,
+  centerPadding: '60px',
+  nextArrow:
+    '<button type="button" class="slick-next"><img style="transform: rotate(180deg)" src="/img/icon/slider-arrow.svg"></button>',
+  prevArrow:
+    '<button type="button" class="slick-prev"><img src="/img/icon/slider-arrow.svg"></button>',
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 550,
+      settings: {
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
