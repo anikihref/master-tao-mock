df40623ea6e553d74d8e67cfa5174f08f8046121
Anton Stecyura <grandwar39@gmail.com>
2022-05-14 15:28:17 +0300

    style form block

--- src/script/main.js
+++ src/script/main.js
@@ -1,21 +1,79 @@
-import $ from 'jquery'; // eslint-disable-next-line
-import 'slick-carousel';
+const deleteFormImg = document.querySelector('.form-step__delete-image-btn');
+const imgState = document.querySelector('.form-step__image-state');
+const imgInput = document.querySelector('.form-step__image-input');
 
-const sliderArrow = document.createElement('div');
-sliderArrow.textContent = 'prevaas';
+function clearImgInputValue() {
+  imgState.textContent = 'Добавьте фотографию';
+  imgState.classList.remove('active');
+  imgInput.value = '';
+}
 
-$('.header-slider').slick({
-  dots: true,
-  nextArrow:
-    '<button type="button" class="slick-next"><img style="transform: rotate(180deg)" src="/img/icon/slider-arrow.svg"></button>',
-  prevArrow:
-    '<button type="button" class="slick-prev"><img src="/img/icon/slider-arrow.svg"></button>',
-  responsive: [
-    {
-      breakpoint: 1440,
-      settings: {
-        appendArrows: '',
-      },
-    },
-  ],
+imgInput.addEventListener('change', (e) => {
+  imgState.textContent = 'Фотография добавлена';
+  imgState.classList.add('active');
+
+  if (!e.target.value) {
+    clearImgInputValue();
+  }
+});
+
+deleteFormImg.addEventListener('click', clearImgInputValue);
+
+const formSteps = [...document.querySelectorAll('.form-step')];
+const form = document.querySelector('.form');
+const TRANSITION_DURATION = 300;
+
+function removeAnimationClasses(currentStep) {
+  formSteps.forEach((step) => {
+    if (step !== currentStep) {
+      step.classList.remove(
+        'fade-in-next',
+        'fade-in-prev',
+        'fade-prev',
+        'fade-next',
+      );
+    }
+  });
+}
+
+form.addEventListener('click', (e) => {
+  const { target } = e;
+  const nextBtn = target.closest('.form-step__next-btn');
+  const prevBtn = target.closest('.form-step__prev-btn');
+
+  if (!nextBtn && !prevBtn) return;
+  e.preventDefault();
+
+  const currentStep = target.closest('.form-step');
+  removeAnimationClasses(currentStep);
+
+  if (nextBtn) {
+    const inputs = [...currentStep.querySelectorAll('input')];
+
+    if (inputs.every((input) => input.reportValidity())) {
+      const nextStep = formSteps[+currentStep.dataset.stepNum + 1];
+
+      removeAnimationClasses(nextStep);
+
+      currentStep.classList.add('fade-next');
+      setTimeout(() => {
+        currentStep.classList.add('hide');
+        nextStep.classList.remove('hide');
+        nextStep.classList.add('fade-in-next');
+      }, TRANSITION_DURATION);
+    }
+  } else if (prevBtn) {
+    const prevStep = formSteps[+currentStep.dataset.stepNum - 1];
+
+    removeAnimationClasses(prevStep);
+
+    currentStep.classList.add('fade-prev');
+    setTimeout(() => {
+      currentStep.classList.add('hide');
+      prevStep.classList.remove('hide');
+      prevStep.classList.add('fade-in-prev');
+    }, TRANSITION_DURATION);
+  }
+
+  removeAnimationClasses(currentStep);
 });
---
+++ src/script/slick-slider.js
@@ -0,0 +1,21 @@
+import $ from 'jquery'; // eslint-disable-next-line
+import 'slick-carousel';
+
+const sliderArrow = document.createElement('div');
+sliderArrow.textContent = 'prevaas';
+
+$('.header-slider').slick({
+  dots: true,
+  nextArrow:
+    '<button type="button" class="slick-next"><img style="transform: rotate(180deg)" src="/img/icon/slider-arrow.svg"></button>',
+  prevArrow:
+    '<button type="button" class="slick-prev"><img src="/img/icon/slider-arrow.svg"></button>',
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        appendArrows: '',
+      },
+    },
+  ],
+});
